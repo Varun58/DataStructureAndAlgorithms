deaa2764c2067ed0ac671bba84013a2a690014fc
Varun Gupta <gvarun1@yahoo.com>
2019-11-19 22:43:30 +0800

    Add new problem stmt for graph: IslandInMatrix

---
+++ Java/src/graph/NumberOfIslands.java
@@ -0,0 +1,47 @@
+package graph;
+
+public class NumberOfIslands {
+  public static void main(String[] args) {
+    char M[][] = new char[][] {
+        {'1', '1', '1', '1', '0'},
+        {'1', '1', '0', '1', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '0', '0', '0'}
+    };
+
+    System.out.println(numIslands(M));
+  }
+
+  public static int numIslands(char[][] grids) {
+    if (grids == null || grids.length == 0) {
+      return 0;
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < grids.length; i++) {
+      for (int j = 0; j < grids[i].length; j++) {
+        if (grids[i][j] == '1') {
+          dfs(grids, i, j);
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  private static void dfs(char[][] grid, int r, int c) {
+    int nr = grid.length;
+    int nc = grid[0].length;
+
+    if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+      return;
+    }
+
+    grid[r][c] = '0';
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+  }
+}
--- Java/src/matrix/IslandInMatrix.java
+++ Java/src/matrix/IslandInMatrix.java
@@ -1,49 +1,50 @@
 package matrix;
 
 public class IslandInMatrix {
-	
-	public static int countNumberOfIslands(int[][] matrix) {
-	
-		if(matrix == null )return 0;
-		
-		boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-		
-		int count=0;
-		
-		for(int i = 0 ;i < matrix.length ;i++) {
-			for(int j = 0 ; j<matrix[0].length;j++) {
-				if(!visited[i][j] && matrix[i][j] == 1)
-				{
-					DFS(matrix,i,j,visited);
-					System.out.println(i+ " "+j);
-					count++;	
-				}
-			}
-		}
-		
-		return count;
-	}
 
-	private static boolean isSafe(int[][] matrix, int row, int col, boolean[][] visited) {
-		return (row>=0 && row<matrix.length && col>=0 
-				&&	col<matrix[0].length
-				&& !visited[row][col]
-			    && matrix[row][col]==1);
-	}
-	
-	private static void DFS(int[][] matrix, int i, int j, boolean[][] visited) {
-		
-		int rowNum[] = {-1,0,-1,1,0,1,1,-1};
-		int colNum[] = {0,-1,-1,0,1,1,-1,1};
-		
-		visited[i][j]=true;
-		
-		for(int k = 0 ;k < 8 ;k++) {
-				if(isSafe(matrix,rowNum[k]+i , colNum[k]+j,visited)) {
-					DFS(matrix,rowNum[k]+i , colNum[k]+j,visited);
-				}
+  public static int countNumberOfIslands(int[][] matrix) {
+
+		if (matrix == null) {
+			return 0;
 		}
-	}
+
+    boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+
+    int count = 0;
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        if (!visited[i][j] && matrix[i][j] == 1) {
+          DFS(matrix, i, j, visited);
+          System.out.println(i + " " + j);
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }
+
+  private static boolean isSafe(int[][] matrix, int row, int col, boolean[][] visited) {
+    return (row >= 0 && row < matrix.length && col >= 0
+        && col < matrix[0].length
+        && !visited[row][col]
+        && matrix[row][col] == 1);
+  }
+
+  private static void DFS(int[][] matrix, int i, int j, boolean[][] visited) {
+
+    int rowNum[] = {-1, 0, -1, 1, 0, 1, 1, -1};
+    int colNum[] = {0, -1, -1, 0, 1, 1, -1, 1};
+
+    visited[i][j] = true;
+
+    for (int k = 0; k < 8; k++) {
+      if (isSafe(matrix, rowNum[k] + i, colNum[k] + j, visited)) {
+        DFS(matrix, rowNum[k] + i, colNum[k] + j, visited);
+      }
+    }
+  }
 	
 /*	public static void main(String[] args) {
 				
