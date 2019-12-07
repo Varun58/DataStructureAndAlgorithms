4710cb534e64b181705b26c73e42f0d3aea2b668
Varun Gupta <gvarun1@yahoo.com>
2019-12-08 00:11:36 +0800

    Add new problem stmt arrays: Calendar 3

---
+++ Java/src/arrays/Calendar3.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.TreeMap;
+
+class Calendar3 {
+
+  TreeMap<Integer, Integer> booked;
+  public Calendar3() {
+    booked = new TreeMap<>();
+  }
+
+  public int book(int start, int end) {
+    booked.put(start, booked.getOrDefault(start, 0) + 1);
+    booked.put(end, booked.getOrDefault(end, 0) - 1);
+
+    int activeEvents = 0, maxActive = 0;
+    for(int f: booked.values()) {
+      maxActive = Math.max(maxActive, activeEvents += f);
+    }
+    return maxActive;
+  }
+}
+
