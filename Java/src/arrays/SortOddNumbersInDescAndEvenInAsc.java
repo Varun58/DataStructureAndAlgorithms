8e77b145a6c95e684a625f30574ee63a1360aaea
Varun Gupta <gvarun1@yahoo.com>
2019-12-17 23:55:26 +0800

    Format the problem sort oldd numbers in desc and even in asc

--- Java/src/arrays/SortOddNumbersInDescAndEvenInAsc.java
+++ Java/src/arrays/SortOddNumbersInDescAndEvenInAsc.java
@@ -7,73 +7,73 @@ import java.util.List;
 
 public class SortOddNumbersInDescAndEvenInAsc {
 
-	public static void main(String[] args) {
-		int arr[] = { 1, 2, 3, 5, 4, 7, 10 };
+  public static void main(String[] args) {
+    int arr[] = {1, 2, 3, 5, 4, 7, 10};
 
-		int res[] = twoWaySortSwappingEvenOddAndSorting(arr);
-		Arrays.stream(res).forEach(x -> System.out.print(x + " "));
-		
-		System.out.println();
-		int arr2[] = { 1, 2, 3, 5, 4, 7, 10 };
-		int res2[] = twoWaySortMakingOddNegativeAndSorting(arr2);
+    int res[] = twoWaySortSwappingEvenOddAndSorting(arr);
+    Arrays.stream(res).forEach(x -> System.out.print(x + " "));
 
-		Arrays.stream(res2).forEach(x -> System.out.print(x + " "));
-	}
+    System.out.println();
+    int arr2[] = {1, 2, 3, 5, 4, 7, 10};
+    int res2[] = twoWaySortMakingOddNegativeAndSorting(arr2);
 
-	static int[] twoWaySortSwappingEvenOddAndSorting(int arr[]) {
-		int l = arr.length;
-		int i = 0;
-		int j = l - 1;
-		int k = 0;
-		while (i < j) {
-			while (arr[i] % 2 != 0) {
-				i++;
-				k++;
-			}
+    Arrays.stream(res2).forEach(x -> System.out.print(x + " "));
+  }
 
-			while (arr[j] % 2 == 0) {
-				j--;
-			}
+  static int[] twoWaySortSwappingEvenOddAndSorting(int arr[]) {
+    int l = arr.length;
+    int i = 0;
+    int j = l - 1;
+    int k = 0;
+    while (i < j) {
+      while (arr[i] % 2 != 0) {
+        i++;
+        k++;
+      }
 
-			if (i < j) {
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
+      while (arr[j] % 2 == 0) {
+        j--;
+      }
 
-		List<Integer> list = new ArrayList<Integer>();
+      if (i < j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
 
-		for (int v = 0; v <k; v++) {
-			list.add(arr[v]);
-		}
-		Collections.sort(list, Collections.reverseOrder());
+    List<Integer> list = new ArrayList<Integer>();
 
-		for (int v = 0; v < list.size(); v++) {
-			arr[v] = list.get(v);
-		}
+    for (int v = 0; v < k; v++) {
+      list.add(arr[v]);
+    }
+    Collections.sort(list, Collections.reverseOrder());
 
-		Arrays.sort(arr, k, l);
+    for (int v = 0; v < list.size(); v++) {
+      arr[v] = list.get(v);
+    }
 
-		return arr;
-	}
+    Arrays.sort(arr, k, l);
 
-	static int[] twoWaySortMakingOddNegativeAndSorting(int arr[]) {
+    return arr;
+  }
 
-		for (int i = 0; i < arr.length; i++) {
-			if ((arr[i] & 1) != 0) {
-				arr[i] *= -1;
-			}
-		}
+  static int[] twoWaySortMakingOddNegativeAndSorting(int arr[]) {
 
-		Arrays.sort(arr);
-		
-		for (int i = 0; i < arr.length; i++) {
-			if ((arr[i] & 1) != 0) {
-				arr[i] *= -1;
-			}
-		}
-		
-		return arr;
-	}
+    for (int i = 0; i < arr.length; i++) {
+      if ((arr[i] & 1) != 0) {
+        arr[i] *= -1;
+      }
+    }
+
+    Arrays.sort(arr);
+
+    for (int i = 0; i < arr.length; i++) {
+      if ((arr[i] & 1) != 0) {
+        arr[i] *= -1;
+      }
+    }
+
+    return arr;
+  }
 }
