c5dbf5db870295154e4b878e7d2c438cd5b51b8e
Varun Gupta <gvarun1@yahoo.com>
2019-12-17 23:54:40 +0800

    Format the problem plus one

--- Java/src/arrays/PlusOne.java
+++ Java/src/arrays/PlusOne.java
@@ -5,52 +5,53 @@ import java.util.Arrays;
 public class PlusOne {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(plusOne(new int[]{1, 8, 9})));
+    System.out.println(Arrays.toString(plusOne(new int[] {1, 8, 9})));
   }
 
   public static int[] plusOne(int[] digits) {
     int carry = 1;
-    for (int i = digits.length-1; i>= 0; i--) {
+    for (int i = digits.length - 1; i >= 0; i--) {
       digits[i] += carry;
-      if (digits[i] <= 9)
+      if (digits[i] <= 9) {
         return digits;
+      }
       digits[i] = 0;
     }
-    int[] ret = new int[digits.length+1];
+    int[] ret = new int[digits.length + 1];
     ret[0] = 1;
     return ret;
   }
 
   public static int[] plusOne2(int[] digits) {
-    if(digits == null || digits.length == 0) {
+    if (digits == null || digits.length == 0) {
       return digits;
     }
 
 
     int carry = 0;
     boolean shift = false;
-    for(int i = digits.length -1 ; i >=0 ; i --) {
-      if(i == digits.length -1 ) {
-        int sum= digits[i] + 1;
-        carry = sum/10;
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (i == digits.length - 1) {
+        int sum = digits[i] + 1;
+        carry = sum / 10;
         digits[i] = sum % 10;
 
-        if(i ==0 && carry != 0) {
+        if (i == 0 && carry != 0) {
           shift = true;
         }
       } else {
-        int sum= digits[i] + carry;
-        carry = sum/10;
+        int sum = digits[i] + carry;
+        carry = sum / 10;
         digits[i] = sum % 10;
 
-        if(i ==0 && carry != 0) {
+        if (i == 0 && carry != 0) {
           shift = true;
         }
       }
     }
 
-    if(shift) {
-      int[] newDigits = new int[digits.length+1];
+    if (shift) {
+      int[] newDigits = new int[digits.length + 1];
       System.arraycopy(digits, 0, newDigits, 1, digits.length);
       newDigits[0] = carry;
       return newDigits;
