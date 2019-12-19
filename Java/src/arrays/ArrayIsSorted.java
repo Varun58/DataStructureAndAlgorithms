3f6f9de8cf9307b28a6991557faf47aa7cff49e5
Varun Gupta <gvarun1@yahoo.com>
2019-12-19 23:37:37 +0800

    Format the classes

--- Java/src/arrays/ArrayIsSorted.java
+++ Java/src/arrays/ArrayIsSorted.java
@@ -4,42 +4,47 @@ import java.util.Arrays;
 
 public class ArrayIsSorted {
 
-	public static boolean checkArrayIsSortedByComparingNextElement(int[] input) {
+  public static boolean checkArrayIsSortedByComparingNextElement(int[] input) {
 
-		if (input == null || input.length <= 1)
+		if (input == null || input.length <= 1) {
 			return true;
-
-		for (int i = 0; i < input.length - 1; i++) {
-			if (input[i] > input[i + 1])
-				return false;
 		}
 
-		return true;
-	}
-	
-	public static boolean checkArrayIsSortedByComparingNextElementUsingCompareTo(Integer[] input) {
-
-		if (input == null || input.length <= 1)
-			return true;
-
-		for (int i = 0; i < input.length - 1; i++) {
-			if (input[i].compareTo( input[i + 1])>0)
+    for (int i = 0; i < input.length - 1; i++) {
+			if (input[i] > input[i + 1]) {
 				return false;
+			}
+    }
+
+    return true;
+  }
+
+  public static boolean checkArrayIsSortedByComparingNextElementUsingCompareTo(Integer[] input) {
+
+		if (input == null || input.length <= 1) {
+			return true;
 		}
 
-		return true;
-	}
+    for (int i = 0; i < input.length - 1; i++) {
+			if (input[i].compareTo(input[i + 1]) > 0) {
+				return false;
+			}
+    }
 
-	public static boolean checkArrayIsSortedUsingSorting(int[] input) {
+    return true;
+  }
 
-		if (input == null || input.length <= 1)
+  public static boolean checkArrayIsSortedUsingSorting(int[] input) {
+
+		if (input == null || input.length <= 1) {
 			return true;
+		}
 
-		int[]  copyArr = Arrays.copyOf(input, input.length);
-		Arrays.sort(copyArr);
-		
-		return Arrays.equals(copyArr,input);
-	}
-	
-	
+    int[] copyArr = Arrays.copyOf(input, input.length);
+    Arrays.sort(copyArr);
+
+    return Arrays.equals(copyArr, input);
+  }
+
+
 }
--- Java/src/arrays/ArrayRotation.java
+++ Java/src/arrays/ArrayRotation.java
@@ -6,77 +6,77 @@ import java.util.List;
 
 public class ArrayRotation {
 
-	public static void main(String[] args) {
-		int b[] =rotateArrayToLeftInKShifts(new int[]{41 ,73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1 ,86, 58, 26, 10, 86, 51}, 10);
-		int  c[] =rotateArrayUsingSystemArrayCopy(new int[]{41 ,73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1 ,86, 58, 26, 10, 86, 51}, 10);
-				
-		
-		for(int b_i=0; b_i < b.length; b_i++){
-            System.out.print(b[b_i]+" ");
-        }
-	
-		System.out.println();
-		System.out.println(Arrays.equals(b, c));
+  public static void main(String[] args) {
+    int b[] = rotateArrayToLeftInKShifts(new int[] {41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51}, 10);
+    int c[] = rotateArrayUsingSystemArrayCopy(new int[] {41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51}, 10);
+
+
+    for (int b_i = 0; b_i < b.length; b_i++) {
+      System.out.print(b[b_i] + " ");
+    }
+
+    System.out.println();
+    System.out.println(Arrays.equals(b, c));
 		/*Input:
 		20 10
 		41 73 89 7 10 1 59 58 84 77 77 97 58 1 86 58 26 10 86 51
 		
 		Output:
 			77 97 58 1 86 58 26 10 86 51 41 73 89 7 10 1 59 58 84 77*/
-	}
-	
-	public static int[] rotateArrayUsingSystemArrayCopy(int[] a, int k) {
-		
-		int n = a.length;
-        int b[] = new int[n];
-		
-        System.arraycopy(a, k, b, 0, n-k);
-		System.arraycopy(a, 0, b, n-k, k);
-		
-		
-		return b;
-	}
-	
-	public static int[] rotateArrayToLeftInKShifts(int[] a,int k) {
-		
-		int n = a.length;
-        int b[] = new int[n];
-        
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        
-        int j=0;
-        for(int i=0;i<n;i++){
-            
-            while(k>0){
-            	list1.add(a[j]);
-                j++;
-                k--;
-            }
-            
-            while(j<n){
-            	list2.add(a[j]);
-                j++;
-            }
-            
-            int li2=0;
-            while(li2<list2.size()){
-               b[i] = list2.get(li2);
-               i++;
-               li2++;
-            }
-            
-            int li =0;
-            while(li<list1.size()){
-               b[i] = list1.get(li);
-               i++;
-               li++;
-            }
-        }
-		return b;
-	}
-	
-	public static int[] rotateArrayToRightInKShifts(int[] arr,int k) {
+  }
+
+  public static int[] rotateArrayUsingSystemArrayCopy(int[] a, int k) {
+
+    int n = a.length;
+    int b[] = new int[n];
+
+    System.arraycopy(a, k, b, 0, n - k);
+    System.arraycopy(a, 0, b, n - k, k);
+
+
+    return b;
+  }
+
+  public static int[] rotateArrayToLeftInKShifts(int[] a, int k) {
+
+    int n = a.length;
+    int b[] = new int[n];
+
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+
+      while (k > 0) {
+        list1.add(a[j]);
+        j++;
+        k--;
+      }
+
+      while (j < n) {
+        list2.add(a[j]);
+        j++;
+      }
+
+      int li2 = 0;
+      while (li2 < list2.size()) {
+        b[i] = list2.get(li2);
+        i++;
+        li2++;
+      }
+
+      int li = 0;
+      while (li < list1.size()) {
+        b[i] = list1.get(li);
+        i++;
+        li++;
+      }
+    }
+    return b;
+  }
+
+  public static int[] rotateArrayToRightInKShifts(int[] arr, int k) {
 		
 		/*int n = in.nextInt();
         int k = in.nextInt();
@@ -117,10 +117,9 @@ public class ArrayRotation {
         for(int b_i=0; b_i < n; b_i++){
             System.out.print(b[b_i]+" ");
         }*/
-		
-		return null;
-	}
 
-	
-	
+    return null;
+  }
+
+
 }
