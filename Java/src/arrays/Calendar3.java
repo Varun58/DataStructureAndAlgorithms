8a88561055b6c418391d6fde41e9f059ff5b530a
Varun Gupta <gvarun1@yahoo.com>
2019-12-19 23:37:47 +0800

    Format the classes

--- Java/src/arrays/Calendar3.java
+++ Java/src/arrays/Calendar3.java
@@ -5,6 +5,7 @@ import java.util.TreeMap;
 class Calendar3 {
 
   TreeMap<Integer, Integer> booked;
+
   public Calendar3() {
     booked = new TreeMap<>();
   }
@@ -14,7 +15,7 @@ class Calendar3 {
     booked.put(end, booked.getOrDefault(end, 0) - 1);
 
     int activeEvents = 0, maxActive = 0;
-    for(int f: booked.values()) {
+    for (int f : booked.values()) {
       maxActive = Math.max(maxActive, activeEvents += f);
     }
     return maxActive;
