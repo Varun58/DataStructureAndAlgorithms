8bf311bf0d9d7f0c54c9d684ad2a329aed2df18e
Varun Gupta <gvarun1@yahoo.com>
2020-01-25 22:00:29 +0800

    Add new solution for checking tree is BST or not

--- Java/src/trees/bst/CheckTreeIsBst.java
+++ Java/src/trees/bst/CheckTreeIsBst.java
@@ -2,31 +2,81 @@ package trees.bst;
 
 import trees.bst.BinarySearchTree.Node;
 
+import java.util.Stack;
+import javax.swing.tree.TreeNode;
+
 public class CheckTreeIsBst {
 
-	public static void main(String[] args) {
-		BinarySearchTree bst = new BinarySearchTree();
+  public static void main(String[] args) {
+    BinarySearchTree bst = new BinarySearchTree();
 
-		bst.root = new Node(4);
+    bst.root = new Node(-2);
 
-		bst.root.left = new Node(2);
-		bst.root.left.left = new Node(1);
-		bst.root.left.right = new Node(3);
+    bst.root.left = new Node(1);
+    bst.root.right = new Node(3);
 
-		bst.root.right = new Node(5);
+    System.out.println(isValidBST(bst.root));
+  }
 
-		System.out.println(isBST(bst.root, Integer.MIN_VALUE, Integer.MAX_VALUE));
+  public static boolean isValidBST(Node root) {
+    return helper(root, null, null);
+  }
+
+  public static boolean helper(Node node, Integer lower, Integer upper) {
+    if (node == null) {
+      return true;
+    }
+
+    int val = node.data;
+    if (lower != null && val <= lower) {
+      return false;
+    }
+    if (upper != null && val >= upper) {
+      return false;
+    }
+
+    if (!helper(node.right, val, upper)) {
+      return false;
+    }
+    if (!helper(node.left, lower, val)) {
+      return false;
+    }
+    return true;
+  }
+
+	public static boolean isValidBST2(Node root) {
+		Stack<Node> stack = new Stack<>();
+		int inOrderValue = Integer.MIN_VALUE;
+
+		while(!stack.isEmpty() || root != null) {
+			while(root != null) {
+				stack.push(root);
+				root = root.left;
+			}
+
+			root = stack.pop();
+			if(root.data <= inOrderValue) {
+				 return false;
+			}
+
+			inOrderValue = root.data;
+			root = root.right;
+		}
+  	return true;
 	}
 
-	static boolean isBST(Node root, int min, int max) {
-		if (root == null) {
-			return true;
-		}
 
-		if (root.data > max || root.data < min) {
-			return false;
-		}
-
-		return isBST(root.left, min, root.data - 1) && isBST(root.right, root.data + 1, max);
-	}
+  // does not work for [-2147483648, -2147483648]
+  // so dont use this solution
+  //static boolean isBST(Node root, int min, int max) {
+  //  if (root == null) {
+  //    return true;
+  //  }
+  //
+  //  if (root.data > max || root.data < min) {
+  //    return false;
+  //  }
+  //
+  //  return isBST(root.left, min, root.data - 1) && isBST(root.right, root.data + 1, max);
+  //}
 }
