62fdec31c4457e5106005b63456a1d51bfd27179
Varun Gupta <gvarun1@yahoo.com>
2020-01-26 09:17:40 +0800

    Add new problem statement in words: Most Common Word

---
+++ Java/src/strings/MostCommonWord.java
@@ -0,0 +1,41 @@
+package strings;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MostCommonWord {
+
+  public String mostCommonWord(String paragraph, String[] banned) {
+    paragraph += ".";
+
+    Set<String> bannedWords = new HashSet<>(Arrays.asList(banned));
+
+    Map<String, Integer> count = new HashMap<>();
+    StringBuilder wordBuilder = new StringBuilder();
+
+    String ansWord = "";
+    int ansWordCount = 0;
+
+    for (Character c : paragraph.toCharArray()) {
+      if (Character.isLetter(c)) {
+        wordBuilder.append(Character.toLowerCase(c));
+      } else if (wordBuilder.length() > 0) {
+        String word = wordBuilder.toString();
+        if (!bannedWords.contains(word)) {
+          count.put(word, count.getOrDefault(word, 0) + 1);
+
+          if (count.get(word) > ansWordCount) {
+            ansWordCount = count.get(word);
+            ansWord = word;
+          }
+        }
+        wordBuilder = new StringBuilder();
+      }
+    }
+
+    return ansWord;
+  }
+}
