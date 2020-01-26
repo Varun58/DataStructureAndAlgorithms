6cc5f5d6cf0f41d5fc7c53ff01110c37b3cef881
Varun Gupta <gvarun1@yahoo.com>
2020-01-26 10:55:39 +0800

    Add new solution for level order tree traversal

--- Java/src/trees/LevelOrderTraversal.java
+++ Java/src/trees/LevelOrderTraversal.java
@@ -1,5 +1,8 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelOrderTraversal {
 
 	public static void main(String[] args) {
@@ -37,6 +40,32 @@ public class LevelOrderTraversal {
 			if (lh > rh)
 				return lh + 1;
 			return rh + 1;
+		}
+	}
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+		DFS(root, 0, results);
+		return results;
+	}
+
+	public void DFS(TreeNode root, int level, List<List<Integer>> results) {
+		if(root == null) {
+			return;
+		}
+		if(level >= results.size()) {
+			List<Integer> newLevel = new ArrayList<>();
+			newLevel.add(root.val);
+			results.add(newLevel);
+		}else {
+			results.get(level).add(root.val);
+		}
+
+		if(root.left!= null) {
+			DFS(root.left, level + 1, results);
+		}
+		if(root.right!=null) {
+			DFS(root.right, level + 1, results);
 		}
 	}
 }
