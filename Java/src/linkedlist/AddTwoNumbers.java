d2923761619811133eefa439d904ea156bbaf465
Varun Gupta <gvarun1@yahoo.com>
2020-01-28 00:37:18 +0800

    Add new solution for linkedlist: Add two numbers

--- Java/src/linkedlist/AddTwoNumbers.java
+++ Java/src/linkedlist/AddTwoNumbers.java
@@ -26,13 +26,45 @@ public class AddTwoNumbers {
     final ListNode result = addTwoNumbers(d1, dd1);
 
     ListNode temp = result;
-    while (temp!= null) {
+    while (temp != null) {
       System.out.print(temp.val + " ");
       temp = temp.next;
     }
   }
 
   public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode(0);
+
+    ListNode curr = dummyHead;
+    ListNode p = l1;
+    ListNode q = l2;
+    int carry = 0;
+
+    while (p != null || q != null) {
+      int x = p != null ? p.val : 0;
+      int y = q != null ? q.val : 0;
+      int sum = x + y + carry;
+      carry = sum / 10;
+      curr.next = new ListNode(sum % 10);
+      curr = curr.next;
+
+      if (p != null) {
+        p = p.next;
+      }
+
+      if (q != null) {
+        q = q.next;
+      }
+    }
+
+    if (carry != 0) {
+      curr.next = new ListNode(carry);
+    }
+
+    return dummyHead.next;
+  }
+
+  public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
     ListNode current1 = l1;
     ListNode current2 = l2;
 
@@ -67,7 +99,7 @@ public class AddTwoNumbers {
       tempNode = newNode;
     }
 
-    if(carry > 0) {
+    if (carry > 0) {
       tempNode.next = new ListNode(carry);
     }
 
