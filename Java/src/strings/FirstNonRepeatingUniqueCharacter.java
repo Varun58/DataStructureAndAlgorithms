d4a94a6bf8519234eca4c48d2ab8be5c854e364f
Varun Gupta <gvarun1@yahoo.com>
2020-01-28 08:33:38 +0800

    Add new problem in strings: First NOn Repaeating Unique Character

---
+++ Java/src/strings/FirstNonRepeatingUniqueCharacter.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNonRepeatingUniqueCharacter {
+
+  public int firstUniqChar(String s) {
+    Map<Character, Integer> count = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      count.put(s.charAt(i), count.getOrDefault(s.charAt(i), 0) + 1);
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+      if(count.get(s.charAt(i)) == 1) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
